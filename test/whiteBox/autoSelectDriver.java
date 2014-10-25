0550d78ef1858021e822715a2b8cab9296f40134
Nhat-Quang Dao <nhat.quang.d@gmail.com>
2014-10-25 15:26:31 -0400

    test packages

---
+++ test/whiteBox/autoSelectDriver.java
@@ -0,0 +1,15 @@
+package whiteBox;
+
+import grl.EvaluationStrategy;
+
+public class autoSelectDriver {
+	EvaluationStrategy strategy;
+	
+	public autoSelectDriver() {
+		
+	}
+	
+	public EvaluationStrategy getStrategy() {
+		return strategy;
+	}
+}
