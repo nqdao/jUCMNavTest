324b08ac3eb376a2c99907c96062fbbdd410cdc7
Nhat-Quang Dao <nhat.quang.d@gmail.com>
2014-10-25 17:26:23 -0400

    nullpointer test

--- test/whiteBox/autoSelectDriver.java
+++ test/whiteBox/autoSelectDriver.java
@@ -1,9 +1,11 @@
 package whiteBox;
 
+import seg.jUCMNav.strategies.FeatureModelStrategyAlgorithm;
 import grl.EvaluationStrategy;
 
 public class autoSelectDriver {
 	EvaluationStrategy strategy;
+	FeatureModelStrategyAlgorithm algo;
 	
 	public autoSelectDriver() {
 		
